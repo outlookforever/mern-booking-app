14e8f38ea87e2cd1c6929f51f497522a4c30e240
Sergey Kochetov <anashkagonit@gmail.com>
2024-02-06 20:05:48 +0300

    add path to index.html in backend for hub-services

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,6 +41,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/my-hotels', myHotelsRoutes)
 
+// TODO: Для Облачных серверов, чтоб работала с одного сервера
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
+})
+
 app.listen(4200,() => {
     console.log('Sever running on port 4200');
 }) 
