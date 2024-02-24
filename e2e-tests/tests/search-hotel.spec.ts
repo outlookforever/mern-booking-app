c4c6be97622315c1adbf57d1ae20beb880ecc14e
Sergey Kochetov <anashkagonit@gmail.com>
2024-02-24 17:48:47 +0300

    add test should details page

--- e2e-tests/tests/search-hotel.spec.ts
+++ e2e-tests/tests/search-hotel.spec.ts
@@ -29,3 +29,15 @@ test("Should show hotel search result", async ({page}) => {
   await expect(page.getByText('Dublin Getaways')).toBeVisible()
 
 })
+
+test('should show hotels details', async({page}) => {
+  await page.goto(UI_URL)
+
+  await page.getByPlaceholder("Where are you going").fill('Dublin')
+  await page.getByRole('button', {name: 'Search'} ).click()
+
+  await page.getByText('Dublin Getaways').click()
+
+  await expect(page).toHaveURL(/detail/)
+  await expect(page.getByRole("button", {name: "Book Now"})).toBeVisible()
+})
